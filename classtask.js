a69c81fcbdf6e2dd0d7fa2693aa7df4d877ebeec
vidhyavkm <85151120+vidhyavkm@users.noreply.github.com>
2022-07-10 11:48:36 +0530

    Add files via upload

---
+++ classtask.js
@@ -0,0 +1,20 @@
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all', true);
+
+// request.send();
+
+// request.onload = function(){
+
+//     var data = JSON.parse(request.response)
+//     console.log(data);
+
+
+// //extract all the details of the countries whose region is "africa" by using filter.
+// //extract only the names of the countries whose population is less than 5 lakhs.
+
+// var result = data.filter((ele) => {
+//     return ele.region === "Africa";
+// });
+// console.log(result);
+//}
---
+++ script.js
@@ -0,0 +1,106 @@
+
+//sum of an array
+//for loop
+//index value adition
+//print
+// sum += arr[i];
+// let sum=0;
+// for (let i=0; i<arr.length;i++){
+//     sum = sum+arr[i];
+// }
+// console.log(sum);
+
+//multiply by 2:
+// let result= [];
+// for (let i=0; i<arr.length;i++){
+// let mulvalue = arr[i]*2;
+// result.push(mulvalue);
+// }
+// console.log(result);
+
+// var arr = [1,2,3,4,5,6];
+// let result = arr.map((element) => {
+// return element*2;
+// });
+// console.log(result);
+
+
+//marks plus 20
+
+// let student = [{name:"Ajay", marks:390},
+// {name:"Anees", marks:410},
+// {name:"Geetha", marks:450},
+// {name:"Saro", marks:430},
+// {name:"Tom", marks:260}];
+
+// let studentArr = student.map((studata)=> {
+//     studata.marks= studata.marks+20
+//     return studata;
+// })
+// console.log(studentArr);
+
+// let studentArr = student.map((studata)=> {
+//         return studata.marks+20;
+//     })
+//     console.log(studentArr);
+// var arr = [1,2,3,4,5,6,7];
+// //print only odd numbers 
+// //print only even numbers
+
+// let result = arr.filter((ele)=>{ 
+//     return ele%2!=0;
+// })
+// console.log(result);
+
+// let result1 = arr.filter((ele)=>{ 
+//     return ele%2===0;
+// })
+// console.log(result1);
+
+// let student = [{name:"Ajay", marks:390},
+// {name:"Anees", marks:410},
+// {name:"Geetha", marks:450},
+// {name:"Saro", marks:430},
+// {name:"Tom", marks:260}];
+
+// // print who got above 400 using filter
+// let studentArr = student.filter((studata)=> {
+//         return studata.marks>=400 
+//     })
+//     console.log(studentArr);
+
+// var arr = [1,2,3,4,5,6];
+// //sum of array
+
+// let sum = arr.reduce((acc, curr)=>{
+//     acc=0;
+//     return acc+curr;
+// });
+// console.log(sum);
+
+// let myArray = [1, 2, 3, 4];
+//     myArray.forEach((value, number) => {
+//         myArray[number] = value-2;
+//     })
+//     console.log(myArray);
+// let num = 4;
+
+//  let arr = [5,6,7];
+//  arr.forEach(num => {
+//     console.log(num);
+//  });
+
+
+//     var arr = [1,2,3,4,5,6];
+// let result = arr.map((element) => {
+// return element*2;
+// });
+// console.log(result);
+
+
+    
+
+
+
+
+
